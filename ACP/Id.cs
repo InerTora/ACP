833cc31a3e0a7819a76a9bda01ac95b144a5e30d
Dev-AynerTora <irenetura1992@gmail.com>
2023-12-15 17:35:40 +0800

    id3

--- ACP/Id.cs
+++ ACP/Id.cs
@@ -8,6 +8,6 @@ namespace ACP
 {
     class Id
     {
-        public static string globalID, globalString, globalString2, suppID, addressID, sdGroupID, button ,tora, ching, tablazon;
+        public static string globalID, globalString, globalString2, suppID, addressID, sdGroupID, button ,tora, ching, tablazon ,AYNER ,TORA;
     }
 }
